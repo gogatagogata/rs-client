c15499168b4f2ff3a6f245acafc2eb0a5d8f1b70
gogatagogata <georgi.zhv@gmail.com>
2023-11-10 08:48:07 +0300

    tif rendering

--- src/app/component/map/map.component.ts
+++ src/app/component/map/map.component.ts
@@ -4,7 +4,7 @@ import View from "ol/View";
 import Map from "ol/Map";
 import OSM from "ol/source/OSM";
 import TileArcGISRest from "ol/source/TileArcGISRest";
-import {FormControl} from "@angular/forms";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: 'map-component',
@@ -14,12 +14,6 @@ import {FormControl} from "@angular/forms";
 export class MapComponent implements OnInit {
 
   osmSource = new OSM();
-  naipSource = new TileArcGISRest({
-    url: "https://naip.imagery1.arcgis.com/arcgis/rest/services/NAIP/ImageServer",
-  });
-  landsatSource = new TileArcGISRest({
-    url: "https://landsat2.arcgis.com/arcgis/rest/services/Landsat/MS/ImageServer",
-  });
   layer = new TileLayer();
   map: Map = new Map();
 
@@ -33,34 +27,32 @@ export class MapComponent implements OnInit {
       }),
       layers: [
         new TileLayer({
-          source: this.osmSource,
-          //   extent: [
-          //     2811031.5665185726, 5393107.291675044, 2821657.957030993,
-          //     5398771.537332108,
-          //   ],
+          source: this.osmSource
         }),
         this.layer,
       ],
       target: "map",
     });
-
-
   }
 
-  selectionChange(selectedLayer: any) {
+  onChangeLayer(selectedLayer: any) {
     if (selectedLayer === "NAIP") {
-      this.layer.setSource(this.naipSource);
+      this.layer.setSource(
+        new TileArcGISRest({ url: "https://naip.imagery1.arcgis.com/arcgis/rest/services/NAIP/ImageServer" })
+      );
     }
     if (selectedLayer === "LANDSAT") {
-      this.layer.setSource(this.landsatSource);
+      this.layer.setSource(
+        new TileArcGISRest({ url: "https://landsat2.arcgis.com/arcgis/rest/services/Landsat/MS/ImageServer" })
+      );
     }
     if (selectedLayer === "OPENMAPS") {
       this.layer.setSource(this.osmSource);
     }
   }
 
-  logCoordinates(coordinate: any) {
-    console.log(coordinate);
+  logCoordinates(event: any) {
+    console.log(this.map.getEventCoordinate(event));
   }
 
 }
--- src/app/component/other/other.component.ts
+++ src/app/component/other/other.component.ts
@@ -1,13 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from "ol/Map";
+import { GeoTIFF } from "ol/source";
+import { createEmpty, getCenter } from "ol/extent";
+import { extend } from "ol/array";
+import { transformExtent } from "ol/proj";
+import TileLayer from "ol/layer/WebGLTile.js";
+import proj4 from "proj4";
+import { register } from "ol/proj/proj4.js";
 
 @Component({
   selector: 'other-component',
   templateUrl: './other.component.html',
   styleUrls: ['./other.component.css']
 })
-export class OtherComponent {
+export class OtherComponent  implements OnInit {
+
+  map: Map = new Map();
+  channels = ["red", "green", "blue"];
+  sourceNames = ["B04", "B03", "B02", "B08"];
+  sources = [
+    new GeoTIFF({
+      sources: this.sourceNames.map(function (name) {
+        return {
+          url: `src/data/response.tif`
+        };
+      }),
+    }),
+  ];
+
+  layer = new TileLayer({
+    sources: this.sources,
+    style: {
+      variables: this.getVariables(),
+      color: [
+        "array",
+        ["band", ["var", "red"]],
+        ["band", ["var", "green"]],
+        ["band", ["var", "blue"]],
+        ["band", 5],
+      ],
+    },
+  });
 
 
+  ngOnInit(): void {
+    proj4.defs("EPSG:4326", "+proj=utm +zone=31 +datum=WGS84 +units=m +no_defs");
+    proj4.defs("EPSG:4326", "+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs");
+    register(proj4);
+
+    this.map = new Map({
+      target: "map",
+      layers: [this.layer],
+      view: Promise.all(
+        this.sources.map(function (source) {
+          return source.getView();
+        })
+      ).then(function (options) {
+        const projection = "EPSG:4326";
+        const extent = createEmpty();
+        options.forEach(function (options) {
+          extend(
+            extent,
+            transformExtent(options.extent!, options.projection, projection)
+          );
+        });
+        return {
+          projection: projection,
+          center: getCenter(extent),
+          zoom: 0,
+          extent: extent,
+        };
+      }),
+    });
+  }
+
+  private getVariables() {
+    const variables: any = {};
+    for (const channel of this.channels) {
+      const selector = document.getElementById(channel);
+      variables[channel] = parseInt((selector as HTMLInputElement).value, 10);
+    }
+    return variables;
+  }
 
 }
 
